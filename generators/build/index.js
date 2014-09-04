8422fc136842c3f1743880e20d4aaa0d54d4c3f5
Marcos Moura <marcosvmmoura@gmail.com>
2014-09-04 02:37:46 -0300

    Init creation of a sub generator for build

---
+++ generators/build/index.js
@@ -0,0 +1,29 @@
+(function() {
+
+    'use strict';
+
+    var yeoman = require('yeoman-generator'),
+        chalk = require('chalk');
+
+    var BuildGenerator = module.exports = yeoman.generators.Base.extend({
+
+        constructor: function () {
+            yeoman.generators.Base.apply(this, arguments);
+        },
+
+        initTask: function() {
+            this.log(chalk.cyan('\t \t  ___           __  __     _    _ '));
+            this.log(chalk.cyan('\t \t / __| __ __ _ / _|/ _|___| |__| |'));
+            this.log(chalk.cyan('\t \t \\__ \\/ _/ _` |  _|  _/ _ \\ / _` |'));
+            this.log(chalk.cyan('\t \t |___/\\__\\__,_|_| |_| \\___/_\\__,_| \n'));
+            this.log(chalk.cyan('\t \t [        Generating Build       ] \n \n'));
+            this.log(chalk.green('Wait until build finish. After all I will create a zip file with all contents of your project. \n \n'));
+        },
+
+        buildTask: function() {
+            this.spawnCommand('grunt', ['build']);
+        }
+
+    });
+
+})();
