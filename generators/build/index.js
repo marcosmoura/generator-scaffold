add101906d1dfb66dd9dc5a3e902178f81e88750
Marcos Moura <marcos.rufino@pontomobi.com.br>
2014-09-04 18:58:51 -0300

    Ask for version change at start

--- generators/build/index.js
+++ generators/build/index.js
@@ -12,17 +12,13 @@
         },
 
         initializing: function() {
-            var done = this.async();
-
             this.log(chalk.cyan('\t \t  ___           __  __     _    _ '));
             this.log(chalk.cyan('\t \t / __| __ __ _ / _|/ _|___| |__| |'));
             this.log(chalk.cyan('\t \t \\__ \\/ _/ _` |  _|  _/ _ \\ / _` |'));
             this.log(chalk.cyan('\t \t |___/\\__\\__,_|_| |_| \\___/_\\__,_| \n'));
             this.log(chalk.cyan('\t \t [        Generating Build       ] \n \n'));
             this.log(chalk.green('Wait until build finish. I will create a zip file with all contents of your project.\n'));
-            this.log(chalk.green('At the end of the process choose which type of version you are generating. \n \n'));
-
-            this.spawnCommand('grunt', ['build']).on('exit', done);
+            this.log(chalk.green('Choose below if this build means a new version and what type. \n \n'));
         },
 
         prompting: function() {
@@ -42,8 +38,6 @@
                         return answers.hasVersion;
                     }
                 }];
-
-            this.log('\n \n');
 
             this.prompt(prompts, function(props) {
                 for(var item in props) {
@@ -71,6 +65,12 @@
 
                 this.spawnCommand('grunt', ['bump:' + version]).on('exit', done);
             }
+        },
+
+        install: function() {
+            var done = this.async();
+
+            this.spawnCommand('grunt', ['build']).on('exit', done);
         },
 
         end: function() {
