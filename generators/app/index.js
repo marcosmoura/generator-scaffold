648f766aa150a256b083edf94abd61d6e0fec047
Marcos Moura <marcosvmmoura@gmail.com>
2014-09-04 02:37:06 -0300

    Create a fancy name

--- generators/app/index.js
+++ generators/app/index.js
@@ -19,7 +19,7 @@
         },
         gift = require('gift');
 
-    module.exports = yeoman.generators.Base.extend({
+    var AppGenerator = module.exports = yeoman.generators.Base.extend({
         constructor: function () {
             yeoman.generators.Base.apply(this, arguments);
 
