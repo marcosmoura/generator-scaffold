8439674e90e3523104c2000c6ea636439f6c654d
Marcos Moura <marcosvmmoura@gmail.com>
2014-09-04 04:01:29 -0300

    Include a beautiful bump version
    Increase the package.json and bower.json with a user choice

--- generators/build/index.js
+++ generators/build/index.js
@@ -11,7 +11,9 @@
             yeoman.generators.Base.apply(this, arguments);
         },
 
-        initTask: function() {
+        initializing: function() {
+            var done = this.async();
+
             this.log(chalk.cyan('\t \t  ___           __  __     _    _ '));
             this.log(chalk.cyan('\t \t / __| __ __ _ / _|/ _|___| |__| |'));
             this.log(chalk.cyan('\t \t \\__ \\/ _/ _` |  _|  _/ _ \\ / _` |'));
@@ -19,10 +21,60 @@
             this.log(chalk.cyan('\t \t [        Generating Build       ] \n \n'));
             this.log(chalk.green('Wait until build finish. I will create a zip file with all contents of your project.\n'));
             this.log(chalk.green('At the end of the process choose which type of version you are generating. \n \n'));
+
+            this.spawnCommand('grunt', ['build']).on('exit', done);
         },
 
-        buildTask: function() {
-            this.spawnCommand('grunt', ['build']);
+        prompting: function() {
+            var cb = this.async(),
+                prompts = [{
+                    type: 'confirm',
+                    name: 'hasVersion',
+                    message: 'This will increase a version of your project?',
+                    default: 0
+                }, {
+                    type: 'list',
+                    name: 'versionType',
+                    message: 'What kind of version?',
+                    choices: ['Just a fix (Ex: 0.0.1)', 'Minor Version (Ex: 0.1.0)', 'Major Version (Ex: 1.0.0)', 'Pre-release (Ex: 1.0.0-1)'],
+                    default: 0,
+                    when: function (answers) {
+                        return answers.hasVersion;
+                    }
+                }];
+
+            this.log('\n \n');
+
+            this.prompt(prompts, function(props) {
+                for(var item in props) {
+                    this[item] = props[item];
+                }
+
+                this.log(chalk.yellow(' \n Perfect! \n \n'));
+
+                cb();
+            }.bind(this));
+        },
+
+        writing: function() {
+            if (this.hasVersion) {
+                var done = this.async(),
+                    version = 'patch';
+
+                if (this.versionType === 'Minor Version (Ex: 0.1.0)') {
+                    version = 'minor';
+                } else if (this.versionType === 'Major Version (Ex: 1.0.0)') {
+                    version = 'major';
+                } else if (this.versionType === 'Pre-release (Ex: 1.0.0-1)') {
+                    version = 'prerelease';
+                }
+
+                this.spawnCommand('grunt', ['bump:' + version]).on('exit', done);
+            }
+        },
+
+        end: function() {
+            this.log(chalk.yellow('\n \n All done! Everything looks fine. Good job! \n \n'));
         }
 
     });
