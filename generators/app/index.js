8956beeef62389ee3e3675a9f3a85db79a853f7c
Marcos Moura <marcos.rufino@pontomobi.com.br>
2015-02-02 20:06:51 -0200

    Update fastclick

--- generators/app/index.js
+++ generators/app/index.js
@@ -285,7 +285,7 @@
                 name: this._.slugify(this.projectName.toLowerCase()),
                 dependencies: {
                     'jquery': '~2.1.3',
-                    'fastclick': '~1.0.3',
+                    'fastclick': '~1.0.6',
                     'modernizr': '~2.8.3',
                     'normalize-css': '~3.0.2'
                 }
