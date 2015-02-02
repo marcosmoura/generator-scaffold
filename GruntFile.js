e9c7b7f6787cd5535686d0c5a2c658d7b12edda7
Marcos Moura <marcos.rufino@pontomobi.com.br>
2015-02-02 19:03:09 -0200

    Add jshint on start

--- GruntFile.js
+++ GruntFile.js
@@ -40,6 +40,9 @@ module.exports = function (grunt) {
 
     grunt.registerTask('test', ['jshint']);
 
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', [
+        'jshint',
+        'watch'
+    ]);
 
 };
