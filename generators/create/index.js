bec76c3d50a07b4a19bcdeaa9099b20eb377d894
Marcos Moura <marcos.rufino@pontomobi.com.br>
2015-02-03 14:05:15 -0200

    Remove all assemble verifications

--- generators/create/index.js
+++ generators/create/index.js
@@ -25,109 +25,95 @@
         },
 
         initializing: function() {
-            if (this.config.get('hasAssemble')) {
-                scaffold.welcomeMessage('        Creating Page        ', 'This is a guide to create a new page for assemble template. It also include a less file.');
-            }
+            scaffold.welcomeMessage('        Creating Page        ', 'This is a guide to create a new page for assemble template. It also include a less file.');
         },
 
         promptTask: function() {
-            if (this.config.get('hasAssemble')) {
-                var done = this.async(),
-                    prompts = [{
-                        name: 'projectName',
-                        message: 'Page name:',
-                        validate: function(input) {
-                            scaffold.validatePrompt({
-                                done: this.async(),
-                                input: input,
-                                message: 'Hey dude! You forgot to enter the page name!'
-                            });
-                        }
-                    }, {
-                        name: 'projectDescription',
-                        message: 'Page description:',
-                        validate: function(input) {
-                            scaffold.validatePrompt({
-                                done: this.async(),
-                                input: input,
-                                message: 'Hey dude! You forgot to enter the project Description!'
-                            });
-                        }
-                    }, {
-                        type: 'confirm',
-                        name: 'createJs',
-                        message: 'Do you like to create a JS file for this page?',
-                        default: 1
-                    }];
-
-                this.prompt(prompts, function(answers) {
-                    for(var answer in answers) {
-                        this[answer] = answers[answer];
+            var done = this.async(),
+                prompts = [{
+                    name: 'projectName',
+                    message: 'Page name:',
+                    validate: function(input) {
+                        scaffold.validatePrompt({
+                            done: this.async(),
+                            input: input,
+                            message: 'Hey dude! You forgot to enter the page name!'
+                        });
                     }
+                }, {
+                    name: 'projectDescription',
+                    message: 'Page description:',
+                    validate: function(input) {
+                        scaffold.validatePrompt({
+                            done: this.async(),
+                            input: input,
+                            message: 'Hey dude! You forgot to enter the project Description!'
+                        });
+                    }
+                }, {
+                    type: 'confirm',
+                    name: 'createJs',
+                    message: 'Do you like to create a JS file for this page?',
+                    default: 1
+                }];
 
-                    this.pageSlug = this._.slugify(this.projectName);
+            this.prompt(prompts, function(answers) {
+                for(var answer in answers) {
+                    this[answer] = answers[answer];
+                }
 
-                    this.log(chalk.yellow(' \nGood! Now I will create all files. Wait a sec. \n \n'));
+                this.pageSlug = this._.slugify(this.projectName);
 
-                    done();
-                }.bind(this));
-            }
+                this.log(chalk.yellow(' \nGood! Now I will create all files. Wait a sec. \n \n'));
+
+                done();
+            }.bind(this));
         },
 
         html: function() {
-            if (this.config.get('hasAssemble')) {
-                this.sourceRoot(path.join(__dirname, '../../templates/web'));
+            this.sourceRoot(path.join(__dirname, '../../templates/web'));
 
-                this.fs.copyTpl(
-                    this.templatePath('index.html'),
-                    this.destinationPath('dev/'+ this.pageSlug +'.html'),
-                    this
-                );
-            }
+            this.fs.copyTpl(
+                this.templatePath('index.html'),
+                this.destinationPath('dev/'+ this.pageSlug +'.html'),
+                this
+            );
         },
 
         css: function() {
-            if (this.config.get('hasAssemble')) {
-                this.sourceRoot(path.join(__dirname, '../../templates/assets/less/pages'));
+            this.sourceRoot(path.join(__dirname, '../../templates/assets/less/pages'));
 
-                this.fs.copy(
-                    this.templatePath('index.less'),
-                    this.destinationPath('dev/assets/less/pages/'+ this.pageSlug +'.less')
-                );
+            this.fs.copy(
+                this.templatePath('index.less'),
+                this.destinationPath('dev/assets/less/pages/'+ this.pageSlug +'.less')
+            );
 
-                mainPath = this.destinationPath('dev/assets/less/main.less');
+            mainPath = this.destinationPath('dev/assets/less/main.less');
 
-                main = this.fs.read(mainPath);
-            }
+            main = this.fs.read(mainPath);
         },
 
         main: function() {
-            if (this.config.get('hasAssemble')) {
-                fs.unlinkSync(mainPath);
+            fs.unlinkSync(mainPath);
 
-                main += '@import \'pages/' + this.pageSlug +'\'\n';
+            main += '@import \'pages/' + this.pageSlug +'\'\n';
 
-                this.fs.write(mainPath, main);
-            }
+            this.fs.write(mainPath, main);
         },
 
         js: function() {
-            if (this.config.get('hasAssemble')) {
-                if (this.createJs) {
-                    this.sourceRoot(path.join(__dirname, '../../templates/assets/js'));
+            if (this.createJs) {
+                this.sourceRoot(path.join(__dirname, '../../templates/assets/js'));
 
-                    this.fs.copy(
-                        this.templatePath('main.js'),
-                        this.destinationPath('dev/assets/js/pages/'+ this.pageSlug +'.js')
-                    );
-                }
+                this.fs.copy(
+                    this.templatePath('main.js'),
+                    this.destinationPath('dev/assets/js/pages/'+ this.pageSlug +'.js')
+                );
             }
         },
 
         end: function() {
-            if (this.config.get('hasAssemble')) {
-                this.log(chalk.yellow('\n \n All done! \n \n'));
-            }
+            this.log(chalk.yellow('\n \n All done! \n \n'));
         }
 
     });
